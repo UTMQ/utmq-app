392b2f989d9d09769b6d64332025dba1c93f47ed
vladikoff <vlad.filippov@gmail.com>
2013-09-26 03:03:18 -0400

    Updating builds, adding misc assets

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,48 @@
+var APP_SRC = './public/**/*';
+
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    // node-webkit build stuff
     nodewebkit: {
       options: {
+        version: '0.7.5',
         build_dir: './build', // Where the build version of my node-webkit app is saved
-        credits: './public/credits.html',
-        mac: true, // We want to build it for mac
-        win: false, // We want to build it for win
-        linux32: true, // We don't need linux32
-        linux64: true, // We don't need linux64
+        credits: './public/credits.html'
       },
-      src: './public/**/*' // Your node-wekit app
+      // the mac version is the default debug build
+      mac: {
+        options: {
+          mac: true,
+          win: false,
+          linux32: false,
+          linux64: false
+        },
+        src: APP_SRC
+      },
+      all: {
+        options: {
+          mac: true,
+          win: true,
+          linux32: true,
+          linux64: true
+        },
+        src: APP_SRC
+      }
     },
+    // watch
+    watch: {
+      scripts: {
+        files: [ APP_SRC ],
+        tasks: [ 'nodewebkit:mac' ]
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-node-webkit-builder');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   grunt.registerTask('default', ['nodewebkit']);
+  grunt.registerTask('dev', ['nodewebkit']);
 
 };
--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-utmq
+UTMQ
 ====
 
-This is UTMQ!
+UTMQ - an assessment tool with question authoring capabilities.
+
+## Dev Setup
+
+
+```
+npm install
+```
+
+### Clean Setup
+
+```
+rm -rf node_modules && npm cache clean && npm install
+```
